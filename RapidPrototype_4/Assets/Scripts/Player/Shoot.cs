986eca1ec558aa79a8974198414d0cca3f7a2258
James  Johnstone <jjohnstone2520@gmail.com>
2018-08-31 14:53:10 +1200

    Fixed player jump glitch

--- RapidPrototype_4/Assets/Scripts/Player/Shoot.cs
+++ RapidPrototype_4/Assets/Scripts/Player/Shoot.cs
@@ -13,14 +13,30 @@ public class Shoot : MonoBehaviour {
 
     public float firespeed;
 
+    private int playerNum;
+
+    private void Start()
+    {
+        playerNum = GetComponent<PlayerMovement>().playerNum;
+    }
+
     // Update is called once per frame
     void Update () {
 
         if (Input.GetKeyDown(key))
         {
-            GameObject trash = Instantiate(objectfire, firepoint.transform.position, Quaternion.identity) as GameObject;
-            trash.GetComponent<Rigidbody>().AddForce(transform.up * firespeed);
-            
+            if (playerNum == 1 && Input.GetKeyDown(KeyCode.S)
+                || playerNum == 2 && Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                GameObject trash = Instantiate(objectfire, new Vector3(0,0,0) , Quaternion.identity) as GameObject;
+                trash.GetComponent<Rigidbody>().AddForce(-transform.up * firespeed);
+            }
+            else
+            {
+                GameObject trash = Instantiate(objectfire, firepoint.transform.position, Quaternion.identity) as GameObject;
+                trash.GetComponent<Rigidbody>().AddForce(transform.right * firespeed);
+            }
+
         }
 
     }
