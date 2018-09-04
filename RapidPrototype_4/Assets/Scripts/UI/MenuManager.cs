1664a5ccdd73fdfda2698c86a5855c36fa9afbcf
James81919 <jjohnstone2520@gmail.com>
2018-09-05 10:04:25 +1200

    Added stuff i think

---
+++ RapidPrototype_4/Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public Canvas menuCanvas;
+    public Canvas controlsCanvas;
+
+	public void Button_Menu_Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Button_Menu_Controls()
+    {
+
+    }
+
+    public void Button_Menu_QuitGame()
+    {
+        Application.Quit();
+    }
+}
